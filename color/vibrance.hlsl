8af6cc5c3b9ec4d23225905a8a7659e06adcc132
Nate Whetsell <nathan.whetsell@gmail.com>
2026-09-18 06:17:31 -0400

    Fix vibrance calculation

--- color/vibrance.hlsl
+++ color/vibrance.hlsl
@@ -2,7 +2,7 @@
 contributors: Christian Cann Schuldt Jensen ~ CeeJay.dk
 description: |
     Vibrance is a smart-tool which cleverly increases the intensity of the more muted colors and leaves the already well-saturated colors alone. Prevents skin tones from becoming overly saturated and unnatural. 
-    vibrance from https://github.com/CeeJayDK/SweetFX/blob/master/Shaders/Vibrance.fx 
+    vibrance from https://github.com/CeeJayDK/SweetFX/blob/master/Shaders/SweetFX/Vibrance.fx 
 use: <float3|float4> vibrance(<float3|float4> color, <float> v) 
 license: MIT License (MIT) Copyright (c) 2014 CeeJayDK
 */
@@ -19,7 +19,7 @@ float3 vibrance(in float3 color, in float v) {
     float min_color = mmin(color);
     float sat = max_color - min_color;
     float lum = luma(color);
-    return lerp(float3(lum, lum, lum), color, 1.0 + (v * 1.0 - (sign(v) * sat)));
+    return lerp(float3(lum, lum, lum), color, 1.0 + (v * (1.0 - (sign(v) * sat))));
 
 }
 
